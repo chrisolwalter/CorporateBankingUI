993902bb0dc145c820fd85813ddd6d763d3a9f61
chrisolwalter <chrisolwalter@gmail.com>
2026-03-28 12:20:16 +0400

    Initial Vite React setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Corporate Banking UI</h1>
+      <p>New Single Transfer Prototype</p>
+    </div>
+  );
+}
